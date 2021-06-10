a8a13796005f7a69d7dae5336d23c661133ef5c2
VictorBwD <victor.bogdanowdornelles@gmail.com>
2021-06-10 01:42:13 -0300

    Tela de criação de usuario finalizada

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ export function Header(){
     return (
         <Flex w="100%" as="header" maxWidth={1480} h="20" mx="auto" mt="4" px="6" align="center" >
             <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
-                Dashgo
-                <Text as="span" ml="1" color="pink.500">.</Text>
+                Dash<Text as="span" color="pink.500">.</Text>Insight<Text as="span" ml="1" color="pink.500">!</Text>
+                
             </Text>
 
             <Flex 
