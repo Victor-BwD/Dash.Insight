1e68758a575e1e78422c03b12c32105ee8f48f18
Victor-BwD <victor.bogdanowdornelles@gmail.com>
2021-07-30 00:50:01 -0300

    [ADDED] Responsive sidebar and sidebar drawer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
 import React from 'react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationNav';
 import { Profile } from './Profile';
@@ -7,6 +9,8 @@ import { SearchBox } from './SearchBox';
 
 
 export function Header(){
+    const { onOpen } = useSidebarDrawer()
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -15,6 +19,18 @@ export function Header(){
 
     return (
         <Flex w="100%" as="header" maxWidth={1480} h="20" mx="auto" mt="4" px="6" align="center" >
+            { !isWideVersion && (
+                <IconButton 
+                    aria-label="Open navigation"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen} 
+                    mr="2"
+                >
+                    
+                </IconButton>
+            )}
             <Logo />
            {isWideVersion && <SearchBox />}
             
